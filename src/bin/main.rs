0423b47117f4592630e878a13056267a214dbcde
Jiayu Liu <jiayu@hey.com>
2024-01-29 11:14:49 +0800

    update cli args

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,9 +2,9 @@ use candle_core::{Device, IndexOp, Module};
 use candle_nn::VarBuilder;
 use clap::{Parser, ValueEnum};
 use hf_hub::api::sync::Api;
-
 use log::info;
 use std::path::PathBuf;
+use std::time::Instant;
 use surya::preprocess::{heatmap_to_gray_image, load_image_tensor, read_resized_image};
 use surya::segformer::SemanticSegmentationModel;
 
@@ -32,7 +32,7 @@ impl TryInto<Device> for DeviceType {
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(long, help = "path to image")]
+    #[arg(help = "path to image")]
     image: PathBuf,
     #[arg(
         long,
@@ -43,53 +43,95 @@ struct Args {
     #[arg(
         long,
         default_value = "model.safetensors",
-        help = "model's weights name"
+        help = "model's weights file name"
     )]
-    weights_name: String,
+    weights_file_name: String,
+    #[arg(long, default_value = "config.json", help = "model's config file name")]
+    config_file_name: String,
+    #[arg(long, default_value_t = true, help = "whether to generate heatmap")]
+    generate_heatmap: bool,
+    #[arg(
+        long,
+        default_value_t = true,
+        help = "whether to generate affinity map"
+    )]
+    generate_affinity_map: bool,
+    #[arg(
+        long,
+        default_value = "./surya_output",
+        help = "output directory, each file will be generating a subdirectory under this directory"
+    )]
+    output_dir: PathBuf,
     #[arg(long, value_enum, default_value_t = DeviceType::Cpu)]
     device_type: DeviceType,
 }
 
 impl Args {
-    fn get_var_builder(&self, device: &Device) -> anyhow::Result<VarBuilder> {
+    fn get_model(
+        &self,
+        device: &Device,
+        num_labels: usize,
+    ) -> anyhow::Result<SemanticSegmentationModel> {
         let api = Api::new()?;
         let repo = api.model(self.model_repo.clone());
-        info!("downloading {} from hugging face", &self.model_repo);
-        let model_file = repo.get(&self.weights_name)?;
+        info!("using model from HuggingFace repo {0}", self.model_repo);
+        let model_file = repo.get(&self.weights_file_name)?;
+        info!("using weights file '{0}'", self.weights_file_name);
         let vb = unsafe {
             VarBuilder::from_mmaped_safetensors(&[model_file], candle_core::DType::F32, device)?
         };
-        Ok(vb)
+        let config_file = repo.get(&self.config_file_name)?;
+        info!("using config file '{0}'", self.config_file_name);
+        let config = serde_json::from_str(&std::fs::read_to_string(config_file)?)?;
+        info!("loaded config: {:?}, num_labels {}", config, num_labels);
+        Ok(SemanticSegmentationModel::new(&config, num_labels, vb)?)
     }
 }
 
+const NUM_LABELS: usize = 2;
+
 fn main() -> anyhow::Result<()> {
     env_logger::init();
+
     let args = Args::parse();
+
     let device = args.device_type.try_into()?;
-    let vb = args.get_var_builder(&device)?;
-    let num_labels = 2;
-    info!(
-        "loaded model from {} with weights file {}",
-        args.model_repo, args.weights_name
-    );
+    info!("using device {:?}", device);
+
     let (image, origin_size) = read_resized_image(&args.image)?;
-    info!("image original size {origin_size:?}");
+    info!("image original size (w, h)={origin_size:?}");
     let image = load_image_tensor(image, &device)?;
+
+    // join the output dir with the input image's base name
+    let output_dir = args
+        .image
+        .file_stem()
+        .ok_or_else(|| anyhow::anyhow!("failed to get file stem of {:?}", args.image))?;
+    let output_dir = args.output_dir.join(output_dir);
+    std::fs::DirBuilder::new()
+        .recursive(true)
+        .create(output_dir.clone())?;
+    info!("generating output to {:?}", output_dir);
+
+    let model = args.get_model(&device, NUM_LABELS)?;
+
     let input = image.unsqueeze(0)?;
-    let config = Default::default();
-    let model = SemanticSegmentationModel::new(&config, num_labels, vb)?;
+    let now = Instant::now();
     let segmentation = model.forward(&input)?;
+    info!("inference took {:.3}s", now.elapsed().as_secs_f32());
     let segmentation = segmentation.squeeze(0)?;
-    {
+
+    if args.generate_heatmap {
+        info!("generating heatmap");
         let heatmap = segmentation.i(0)?;
         let imgbuf = heatmap_to_gray_image(heatmap, origin_size)?;
-        imgbuf.save("heatmap.png")?;
+        imgbuf.save(output_dir.join("heatmap.png"))?;
     }
-    {
+    if args.generate_affinity_map {
+        info!("generating affinity map");
         let affinity_map = segmentation.i(1)?;
         let imgbuf = heatmap_to_gray_image(affinity_map, origin_size)?;
-        imgbuf.save("affinity_map.png")?;
+        imgbuf.save(output_dir.join("affinity_map.png"))?;
     }
     Ok(())
 }
