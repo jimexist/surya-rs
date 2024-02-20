c1829849470ab04c4852c1bc39ec4a5320d08f4a
Jiayu Liu <jiayu@hey.com>
2024-02-20 16:49:27 +0800

    prune statements

--- src/hf.rs
+++ src/hf.rs
@@ -13,27 +13,34 @@ pub struct HfModelInfo {
     pub config_file: String,
 }
 
+impl HfModelInfo {
+    pub fn download_model_files(&self) -> Result<(PathBuf, PathBuf)> {
+        let api = ApiBuilder::new().with_progress(true).build()?;
+        let repo = api.model(self.repo.clone());
+        debug!(
+            "using {} model from HuggingFace repo '{}'",
+            self.model_type, self.repo,
+        );
+        let model_file = repo.get(&self.weights_file)?;
+        debug!(
+            "using {} weights file '{}'",
+            self.model_type, self.weights_file
+        );
+        let config_file = repo.get(&self.config_file)?;
+        debug!(
+            "using {} config file '{}'",
+            self.model_type, self.config_file
+        );
+        Ok((config_file, model_file))
+    }
+}
+
 pub trait HfModel {
     fn from_hf(info: HfModelInfo, device: &Device) -> Result<Self>
     where
         Self: Sized,
     {
-        let api = ApiBuilder::new().with_progress(true).build()?;
-        let repo = api.model(info.repo.clone());
-        debug!(
-            "using {} model from HuggingFace repo '{}'",
-            info.model_type, info.repo,
-        );
-        let model_file = repo.get(&info.weights_file)?;
-        debug!(
-            "using {} weights file '{}'",
-            info.model_type, info.weights_file
-        );
-        let config_file = repo.get(&info.config_file)?;
-        debug!(
-            "using {} config file '{}'",
-            info.model_type, info.config_file
-        );
+        let (config_file, model_file) = info.download_model_files()?;
         Self::from_hf_files(config_file, model_file, device)
     }
 
